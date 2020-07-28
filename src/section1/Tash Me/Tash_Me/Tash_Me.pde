724d4cf7bd271f5d9cc8d83b36b56eb5c6602685
root <root@857978baa4d5>
2020-07-28 21:54:39 +0000

    day 2

--- src/section1/Tash Me/Tash_Me/Tash_Me.pde
+++ src/section1/Tash Me/Tash_Me/Tash_Me.pde
@@ -1,8 +1,13 @@
-
+PImage mustache;
+PImage face;
 void setup() {
-
+mustache = loadImage("tash.png");
+  face = loadImage("face.jpg");
+size(800,600);
+face.resize(width,height);
 }
 
 void draw() {
-
+  background(face); 
+image(mustache,mouseX,mouseY);
 }
--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -7,9 +7,17 @@ public class MyFirstJavaProgram {
 	
 	public static void main(String[] args) {
 		
-		// START HERE
-	
-		
-		
+		Robot one = new Robot();
+	one.setRandomPenColor();
+	one.penDown();
+	one.setSpeed(300);
+	for (int i = 0; i < 315; i++) {
+		one.turn(360/359);
+		one.move(2);
+			
 	}
-}
+	
+	
+	
+	}
+	}
